7a1cf1f2c96da5af1b67339a7ae0a2cd4bf1458c
ssudhir6 <sudhirsahil2001@gmail.com>
2020-07-17 11:11:34 -0400

    merged bottomUI viewpager2 and color changes

--- Shushhh/app/src/main/java/com/example/shushandroid/MainActivity.java
+++ Shushhh/app/src/main/java/com/example/shushandroid/MainActivity.java
@@ -1,15 +1,6 @@
 package com.example.shushandroid;
 
 import androidx.annotation.NonNull;
-<<<<<<< HEAD
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-=======
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -17,7 +8,6 @@ import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
->>>>>>> bottomUI
 
 import android.os.Build;
 import android.os.Bundle;
@@ -32,13 +22,9 @@ import java.util.function.Consumer;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main Activity";
-<<<<<<< HEAD
-    private ViewPager viewPager;
-=======
     private ViewPager2 viewPager2;
     private CustomPagerAdapter adapter;
     private ArrayList<Fragment> arrayList;
->>>>>>> bottomUI
     private TabLayout tabLayout;
 
     @Override
@@ -46,51 +32,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
-        viewPager = findViewById(R.id.tabviewpager);
-        tabLayout = findViewById(R.id.tablayout);
-
-        viewPager.setAdapter(new CustomPagerAdapter(getSupportFragmentManager()));
-        tabLayout.setupWithViewPager(viewPager);
-
-    }
-
-    private class CustomPagerAdapter extends FragmentStatePagerAdapter {
-
-        public CustomPagerAdapter(@NonNull FragmentManager fm) {
-            super(fm);
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            if (position == 0)
-                return new PlaceTab();
-            else if (position == 1)
-                return new TimeTab();
-            else return null;
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-        @Nullable
-        @Override
-        public CharSequence getPageTitle(int position) {
-            if (position == 0) {
-                return "Place";
-            } else if (position == 1) {
-                return "Time";
-            } else {
-                return "";
-            }
-        }
-    }
-
-}
-=======
         viewPager2 = findViewById(R.id.tabviewpager);
         tabLayout = findViewById(R.id.tabview);
 
@@ -133,4 +74,3 @@ class CustomPagerAdapter extends FragmentStateAdapter {
     }
 
 }
->>>>>>> bottomUI
