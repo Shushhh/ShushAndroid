22ac43cd0a966318649fb76bdcc583a6ae803757
Akash Veerappan <akashpl2002@outlook.com>
2020-07-16 21:32:29 -0400

    Confirmed patches to ViewPager2. Added new color scheme colors to resources directory and applied changes in main page.

--- Shushhh/app/src/main/java/com/example/shushandroid/MainActivity.java
+++ Shushhh/app/src/main/java/com/example/shushandroid/MainActivity.java
@@ -1,47 +1,45 @@
 package com.example.shushandroid;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.lifecycle.Lifecycle;
-import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main Activity";
     private ViewPager2 viewPager2;
     private CustomPagerAdapter adapter;
-    private ArrayList<Fragment> arrayList = new ArrayList<>();
+    private ArrayList<Fragment> arrayList;
     private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: Starting.");
 
         viewPager2 = findViewById(R.id.tabviewpager);
         tabLayout = findViewById(R.id.tabview);
 
-        arrayList.add(new PlaceTab());
-        arrayList.add(new TimeTab());
+        arrayList = new ArrayList<>(Arrays.asList(new PlaceTab(), new TimeTab()));
 
         adapter = new CustomPagerAdapter(getSupportFragmentManager(), getLifecycle());
         viewPager2.setAdapter(adapter);
+
         new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
             if (position == 0) {
                 tab.setText("Place");
@@ -49,30 +47,30 @@ public class MainActivity extends AppCompatActivity {
                 tab.setText("Time");
             }
         }).attach();
-    }
-
 
     }
 
-    class CustomPagerAdapter extends FragmentStateAdapter {
+}
 
-        public CustomPagerAdapter(@NonNull FragmentManager fm, @NonNull Lifecycle lifecycle) {
-            super(fm, lifecycle);
-        }
+class CustomPagerAdapter extends FragmentStateAdapter {
 
-        @NonNull
-        @Override
-        public Fragment createFragment(int position) {
-            if (position == 0)
-                return new PlaceTab();
-            else if (position == 1)
-                return new TimeTab();
-            else return null;
-        }
-
-        @Override
-        public int getItemCount() {
-            return 2;
-        }
-
+    public CustomPagerAdapter(@NonNull FragmentManager fm, @NonNull Lifecycle lifecycle) {
+        super(fm, lifecycle);
     }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position == 0)
+            return new PlaceTab();
+        else if (position == 1)
+            return new TimeTab();
+        else return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+
+}
