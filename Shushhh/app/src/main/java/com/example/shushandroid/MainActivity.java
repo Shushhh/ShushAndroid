49ae773aa0bcb476537eddb652534a05b9f694f5
Akash Veerappan <akashpl2002@outlook.com>
2020-07-18 00:12:11 -0400

    BottomSheetDialogFragment works. BottomSheetBehavior not needed, as it is a persistent sheet and not a modal dialog (discuss later). New bug: white corners on the BottomSheetDialogFragment (will file other bugs soon).

--- Shushhh/app/src/main/java/com/example/shushandroid/MainActivity.java
+++ Shushhh/app/src/main/java/com/example/shushandroid/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.shushandroid;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -13,10 +15,15 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -33,8 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
 
     private BottomAppBar bottomAppBar;
-    private ConstraintLayout bottomSheet;
-    private BottomSheetBehavior bottomSheetBehavior;
+    private VoicemailBottomSheetDialogFragment voicemailBottomSheetDialogFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,11 +49,18 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager2 = findViewById(R.id.tabviewpager);
         tabLayout = findViewById(R.id.tabview);
+        bottomAppBar = findViewById(R.id.bottomappbar);
+
+        voicemailBottomSheetDialogFragment = new VoicemailBottomSheetDialogFragment();
 
         arrayList = new ArrayList<>(Arrays.asList(new PlaceTab(), new TimeTab()));
 
         adapter = new CustomPagerAdapter(getSupportFragmentManager(), getLifecycle());
         viewPager2.setAdapter(adapter);
+
+        bottomAppBar.setNavigationOnClickListener((View v) -> {
+            voicemailBottomSheetDialogFragment.show(getSupportFragmentManager(), "dialog_fragment");
+        });
 
         new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
             if (position == 0) {
@@ -57,12 +70,19 @@ public class MainActivity extends AppCompatActivity {
             }
         }).attach();
 
-        bottomSheet.findViewById(R.id.bottomsheet);
         bottomAppBar.findViewById(R.id.bottomappbar);
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
 
-        bottomAppBar.setNavigationOnClickListener(view ->
-                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED));
+    }
+
+
+    public static class VoicemailBottomSheetDialogFragment extends BottomSheetDialogFragment {
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.bottom_sheet_voicemail, container, false);
+            getDialog().setCanceledOnTouchOutside(true);
+            return view;
+        }
     }
 
 }
