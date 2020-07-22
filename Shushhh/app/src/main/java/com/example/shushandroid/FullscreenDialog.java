ac9913315fe82f1adc76cb9689ba83389f420cec
Sahil Sudhir <sudhirsahil2001@gmail.com>
2020-07-22 14:14:45 -0400

    buggy datepickerfragment

--- Shushhh/app/src/main/java/com/example/shushandroid/FullscreenDialog.java
+++ Shushhh/app/src/main/java/com/example/shushandroid/FullscreenDialog.java
@@ -1,5 +1,6 @@
 package com.example.shushandroid;
 
+import android.app.DatePickerDialog;
 import android.media.Image;
 import android.os.Bundle;
 
@@ -7,14 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-public class FullscreenDialog extends DialogFragment implements View.OnClickListener {
+public class FullscreenDialog extends DialogFragment implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
     static FullscreenDialog newInstance() {
         return new FullscreenDialog();
@@ -51,9 +57,21 @@ public class FullscreenDialog extends DialogFragment implements View.OnClickList
             case R.id.fullscreen_dialog_action:
                 dismiss();
                 break;
-
+            case R.id.firstdate:
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getFragmentManager(), "date picker");
         }
 
     }
 
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+        TextView textView = (TextView) findViewById(R.id.firstdate);
+        textView.setText(currentDateString);
+    }
 }
