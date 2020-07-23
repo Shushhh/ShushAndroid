7d0fe7f7da7be2314ad7403832c9596fdf7ab24a
Akash Veerappan <akashpl2002@outlook.com>
2020-07-22 23:23:30 -0400

    Working date picker dialog

--- Shushhh/app/src/main/java/com/example/shushandroid/DatePickerFragment.java
+++ Shushhh/app/src/main/java/com/example/shushandroid/DatePickerFragment.java
@@ -3,20 +3,13 @@ package com.example.shushandroid;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.DatePicker;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
-public class DatePickerFragment extends FullscreenDialog {
 
-    @NonNull
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
-        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
-    }
-}
--- Shushhh/app/src/main/java/com/example/shushandroid/FullscreenDialog.java
+++ Shushhh/app/src/main/java/com/example/shushandroid/FullscreenDialog.java
@@ -1,9 +1,12 @@
 package com.example.shushandroid;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.media.Image;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +17,14 @@ import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-public class FullscreenDialog extends DialogFragment implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
+public class FullscreenDialog extends DialogFragment implements View.OnClickListener {
 
     static FullscreenDialog newInstance() {
         return new FullscreenDialog();
@@ -38,9 +42,11 @@ public class FullscreenDialog extends DialogFragment implements View.OnClickList
         View view = inflater.inflate(R.layout.time_dialog, container, false);
         ImageButton close = view.findViewById(R.id.fullscreen_dialog_close);
         Button action = view.findViewById(R.id.fullscreen_dialog_action);
+        TextView textView = view.findViewById(R.id.firstdate);
 
         close.setOnClickListener(this);
         action.setOnClickListener(this);
+        textView.setOnClickListener(this);
 
         return view;
     }
@@ -48,30 +54,45 @@ public class FullscreenDialog extends DialogFragment implements View.OnClickList
     @Override
     public void onClick(View v) {
         int id = v.getId();
-
         switch (id) {
             case R.id.fullscreen_dialog_close:
                 dismiss();
                 break;
-
             case R.id.fullscreen_dialog_action:
                 dismiss();
                 break;
             case R.id.firstdate:
-                DialogFragment datePicker = new DatePickerFragment();
+                DialogFragment datePicker = new DatePickerFragment(getActivity());
                 datePicker.show(getFragmentManager(), "date picker");
         }
-
     }
 
-    @Override
-    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.YEAR, year);
-        c.set(Calendar.MONTH, month);
-        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
-        TextView textView = (TextView) findViewById(R.id.firstdate);
-        textView.setText(currentDateString);
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+        private Context context;
+
+        DatePickerFragment (Context context) {
+            this.context = context;
+        }
+
+        @NonNull
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            return new DatePickerDialog(context, this, year, month, day);
+        }
+
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+            Calendar c = Calendar.getInstance();
+            c.set(Calendar.YEAR, year);
+            c.set(Calendar.MONTH, month);
+            c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+            Log.i("Date", currentDateString);
+        }
     }
+
 }
