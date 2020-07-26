0b2d22e81ca3d7b41df6f6c64817d6610b54dbcc
Sahil Sudhir <sudhirsahil2001@gmail.com>
2020-07-25 20:31:15 -0400

    documentation for Location Dialog

--- Shushhh/app/src/main/java/com/example/shushandroid/LocationDialog.java
+++ Shushhh/app/src/main/java/com/example/shushandroid/LocationDialog.java
@@ -11,18 +11,42 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+/**
+ * @apiNote Location Dialog helper class
+ * @author  Sahil Sudhir
+ * @version 1.0
+ * @since   2020-7-25
+ *
+ * Resources:
+ */
+
 public class LocationDialog extends DialogFragment {
 
+    /**
+     * method that returns a new instance of the LocationDialog class
+     * @return a LocationDialog object instance
+     */
     static LocationDialog newInstance () {
         return new LocationDialog();
     }
 
+    /**
+     * method that creates the dialog
+     * @param savedInstanceState a bundle that contains the necessary items to create an instance of the dialog
+     */
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setStyle(DialogFragment.STYLE_NORMAL, R.style.FullscreenDialogTheme);
     }
 
+    /**
+     * method that creates the view that contains the dialog
+     * @param inflater the layout inflater that opens the layout
+     * @param container the view that the UI should attach to
+     * @param savedInstanceState creates or reuses the instance state of the layout
+     * @return the view shown on screen
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
