397133893816d27db9ddf03076a77f753e111847
Sahil Sudhir <sudhirsahil2001@gmail.com>
2020-07-26 21:55:11 -0400

    added your location detector and map marker feature

--- Shushhh/app/src/main/java/com/example/shushandroid/MapActivity.java
+++ Shushhh/app/src/main/java/com/example/shushandroid/MapActivity.java
@@ -1,19 +1,31 @@
 package com.example.shushandroid;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final float DEFAULT_ZOOM = 15f;
     private GoogleMap map;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     private static final String TAG = "MapActivity";
 
@@ -23,6 +35,32 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_map);
         initMap();
     }
+
+    private void getDeviceLocation() {
+        Log.d(TAG, "getting the device's location");
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            Task getLocation = fusedLocationProviderClient.getLastLocation();
+            getLocation.addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "found location");
+                    Location myLocation = (Location) task.getResult();
+                    moveCamera(new LatLng(myLocation.getLatitude(), myLocation.getLongitude()), DEFAULT_ZOOM);
+                } else {
+                    Log.d(TAG, "current location is not found");
+                    Toast.makeText(MapActivity.this, "unable to find current location", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (SecurityException s) {
+            Log.e(TAG, "Security Exception: " + s.getMessage());
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d(TAG, "moving camera to latitude " + latLng.latitude + " and longitude " + latLng.longitude);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
     private void initMap() {
         Log.d(TAG, "initMap: initializing map");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
@@ -34,5 +72,17 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: map is ready");
         map = googleMap;
+        getDeviceLocation();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true);
     }
 }
