8157bc411a56ce5ca2fd3a23d6ba0298e2e75f90
Akash Veerappan <akashpl2002@outlook.com>
2020-07-27 00:34:22 -0400

    Had to change database version for my local device, hence the commit

--- Shushhh/app/src/main/java/com/example/shushandroid/DatabaseManager.java
+++ Shushhh/app/src/main/java/com/example/shushandroid/DatabaseManager.java
@@ -61,7 +61,7 @@ public class DatabaseManager extends SQLiteOpenHelper {
     }
 
     public DatabaseManager(@Nullable Context context) {
-        super(context, DatabaseEntry.TABLE_NAME, null, 2);
+        super(context, DatabaseEntry.TABLE_NAME, null, 3);
     }
 
     /**
