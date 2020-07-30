b081bca26ea0ad433c46ba252d8ea1c18e8954d5
Akash Veerappan <akashpl2002@outlook.com>
2020-07-29 22:32:33 -0400

    Working foreground service

---
+++ Shushhh/app/src/main/java/com/example/shushandroid/ForegroundServiceManager.java
@@ -0,0 +1,47 @@
+package com.example.shushandroid;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+public class ForegroundServiceManager extends Service {
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification = new Notification.Builder(this, "service")
+                .setContentTitle("Service")
+                .setContentText("Test")
+                .setSmallIcon(R.drawable.ic_baseline_close_24)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
+        return START_NOT_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
--- Shushhh/app/src/main/java/com/example/shushandroid/MainActivity.java
+++ Shushhh/app/src/main/java/com/example/shushandroid/MainActivity.java
@@ -13,6 +13,9 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -112,6 +115,14 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show(getSupportFragmentManager(), ShushObject.ShushObjectType.TIME.getDescription(), "fab");
             }
         });
+
+        serviceTest();
+
+    }
+
+    public void serviceTest() {
+        Intent intent = new Intent(this, ForegroundServiceManager.class);
+        startService(intent);
     }
 
     public static class VoicemailBottomSheetDialogFragment extends BottomSheetDialogFragment {
